2e77a95b0f03cdaea7562a8b1274b15e42a7942d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-02-08 23:34:14 +0700

    chore: app.js

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var webRouter = require("./routes/web");
-var apiRouter = require("./routes/api");
-var directory = require("./config/path");
-var database = require("./config/database");
-var config = require("./config/app");
+const indexRouter = require("./routes/index");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -20,44 +16,24 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require(`./app/http/${directory.modules}/i18n`));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", webRouter);
-app.use("/api", apiRouter);
-
-// development mode
-if (config.env == "local") {
-    var testModule = require(`./${directory.tests}/${directory.modules}/module`);
-
-    app.use(testModule);
-    app.use(require(`./${directory.tests}/${directory.router}`));
-}
-
-// mongoose connection
-if (database.default == "mongoose") {
-    var connectDB = require("./config/mongoose");
-
-    connectDB.connect();
-}
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = config.env === "local" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    if (config.debug) {
-        console.log("Warning you running this with debug mode");
-    }
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
