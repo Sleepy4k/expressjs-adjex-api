04d29d15c234a577e6ee59f21a3a88907f04ae9c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-02-11 03:20:07 +0700

    chore: some seeder

--- database/seeders/20230205024945-demo-quizzes.js
+++ database/seeders/20230205024945-demo-quizzes.js
@@ -1,50 +1,14 @@
 "use strict";
 
+const factories = require("../factories/20230208010223-seed-quizzes");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("quizzes", [
-      {
-        question: "Siapa nama presiden Indonesia saat ini?",
-        a: "Joko Widodo",
-        b: "Susilo Bambang Yudhoyono",
-        c: "Megawati Soekarnoputri",
-        d: "Soeharto",
-        answer: "a",
-        categoryId: 1,
-        levelId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        question: "Siapa nama presiden Indonesia sebelum Joko Widodo?",
-        a: "Joko Widodo",
-        b: "Susilo Bambang Yudhoyono",
-        c: "Megawati Soekarnoputri",
-        d: "Soeharto",
-        answer: "b",
-        categoryId: 1,
-        levelId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        question:
-          "Siapa nama presiden Indonesia sebelum Susilo Bambang Yudhoyono?",
-        a: "Joko Widodo",
-        b: "Susilo Bambang Yudhoyono",
-        c: "Megawati Soekarnoputri",
-        d: "Soeharto",
-        answer: "c",
-        categoryId: 1,
-        levelId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    await queryInterface.bulkInsert("quizzes", factories.up());
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("quizzes", null, {});
+    await queryInterface.bulkDelete("quizzes", factories.down(), {});
   },
 };
