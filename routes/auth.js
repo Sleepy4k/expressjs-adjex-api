ad9aa016981f42388aa6c7d447e8333d8c917a63
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-02-11 13:43:44 +0700

    chore: auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { register } = require("../middleware");
 const authController = require("../controllers/auth.controller");
 
 /* POST login listing. */
