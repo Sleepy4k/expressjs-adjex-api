365253598bbd8d4dbdf229e8c96f9011fafe6658
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-02-24 19:25:39 +0700

    chore: app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const logger = require("morgan");
 const app = express();
 
@@ -15,7 +15,13 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: "session",
+    secret: "secret",
+    httpOnly: true,
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 models.sequelize
