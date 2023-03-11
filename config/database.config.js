14c59998ef9c04f28f85bc92cd1e9e72aa94df8e
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-11 12:18:16 +0700

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DEV_DB_USERNAME || "oxcoqotjqyflvy",
-    password: process.env.DEV_DB_PASSWORD || "5d7939a1c8cac3765cfaf98d39475bea39a85ec28bb7d1e09fb8ff8af336eca3",
-    database: process.env.DEV_DB_NAME || "dccnranh6aslrq",
-    host: process.env.DEV_DB_HOSTNAME || "ec2-3-93-160-246.compute-1.amazonaws.com",
-    port: process.env.DEV_DB_PORT || 5432,
-    dialect: "postgres",
+    username: process.env.DEV_DB_USERNAME || "root",
+    password: process.env.DEV_DB_PASSWORD || "gjxF3ehKgMn044eX5hqHZQqCxdMp1kZY",
+    database: process.env.DEV_DB_NAME || "adjex",
+    host: process.env.DEV_DB_HOSTNAME || "lfbwi0.stackhero-network.com",
+    port: process.env.DEV_DB_PORT || 3306,
+    dialect: "mysql",
     dialectOptions: {
       bigNumberStrings: true,
     },
