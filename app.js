7faac079a55cf05168f358b5d6d319b5b550c61c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-18 22:42:15 +0700

    chore: app.js

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieSession = require("cookie-session");
-const logger = require("morgan");
+/**
+ * Module dependencies.
+ */
+import logger from "morgan";
+import express from "express";
+import createError from "http-errors";
+import cookieSession from "cookie-session";
+
+/**
+ * Import routes
+ */
+import routes from "./routes/index.js";
+
+/**
+ * Import models
+ */
+import models from "./models/index.js";
+
+/**
+ * Create express app
+ */
 const app = express();
 
-const models = require("./models");
-const indexRouter = require("./routes/index");
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
+/**
+ * Setup logger and middlewares
+ */
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,29 +34,41 @@ app.use(
     httpOnly: true,
   })
 );
-app.use(express.static(path.join(__dirname, "public")));
 
+/**
+ * Models initialization
+ */
 models.sequelize
   .authenticate()
   .then(() => console.log("Connection has been established successfully."))
   .catch((err) => console.error("Unable to connect to the database:", err));
 
-app.use("/", indexRouter);
+/**
+ * Routes initialization
+ */
+app.use("/", routes);
 
-// catch 404 and forward to error handler
+/**
+ * Catch 404 and forward to error handler
+ */
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+/**
+ * Error handler
+ */
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).send({
+    status: "error",
+    message: err.message,
+    data: {},
+  });
 });
 
-module.exports = app;
+export default app;
+
+// Path: app.js
